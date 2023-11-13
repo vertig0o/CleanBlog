b12cf03ad781c6e8d6d39d76e76a1a75e36f5704
VerigoOo <atilay_yoruk@outlook.com>
2023-11-13 15:13:53 +0300

    CBP-2

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  const blog = { id: 1, title: "Blog title", description: "Blog description" };
-  res.send(blog);
+  res.render("index");
+});
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+app.get("/add_post", (req, res) => {
+  res.render("add_post");
+});
+app.get("/post", (req, res) => {
+  res.render("post");
 });
 
 const port = 3000;
